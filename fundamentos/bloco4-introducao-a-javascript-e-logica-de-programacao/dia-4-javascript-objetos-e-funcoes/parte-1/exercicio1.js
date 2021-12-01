c36ce227d3b781ecdde9e279b454909ecd6936d4
Thiago William <thiagokepler1@gmail.com>
2021-12-01 19:21:53 -0300

    exercicios parte 1 concluído

---
+++ fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-1/exercicio1.js
@@ -0,0 +1,11 @@
+//Imprima no console uma mensagem de boas-vindas para a personagem abaixo, incluindo seu nome. Valor esperado no console: Bem-vinda, Margarida
+
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+
+console.log('Bem-vinda, %s', info.personagem);
---
+++ fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-1/exercicio5.js
@@ -0,0 +1,40 @@
+//Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+};
+
+let infoDois = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
+for (let chave in info) {
+    if (chave == 'recorrente') {
+        if (infoDois[chave] == 'Sim' && info[chave] == 'Sim'){
+            console.log('Ambos recorrentes');
+        }
+
+        else if (info[chave] == 'Sim') {
+            console.log('Apenas %s recorrente', info.personagem);
+        }
+
+        else if (infoDois[chave] == 'Sim') {
+            console.log('Apenas %s recorrente', infoDois.personagem);
+        }
+
+        else {
+            console.log('Nenhum recorrente');
+        }
+    }
+
+    else {
+        console.log('%s e %s',info[chave], infoDois[chave]);
+    }
+
+} 
