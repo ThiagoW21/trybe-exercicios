7c86326c6d00b57578f4d7af50b1edfdf426a327
Thiago William <thiagokepler1@gmail.com>
2021-12-01 18:11:47 -0300

    Exercicios parte 1 resolvidos

---
+++ dia-4-javascript-objetos-e-funcoes/parte-1/exercicio6.js
@@ -0,0 +1,20 @@
+//Usando o objeto abaixo, faça os exercícios a seguir:
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  //Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+  livro = leitor.livrosFavoritos[0].titulo;[]
+
+  console.log("O livro favorito de Julia Pessoa se chama '%s'", livro)
